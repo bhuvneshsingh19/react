36f3cad19790bf57f83b333326cd662b9ed65811
Bhuvnesh Singh <bhuvnesh.singh_cs.aiml22@gla.ac.in>
2025-07-19 18:55:18 +0530

    set up custom react

--- 01basicvite/src/App.jsx
+++ 01basicvite/src/App.jsx
@@ -2,12 +2,12 @@
 
 import Youtube from "./Youtube"
 
-
 function App() {
+  const username = "bhuvnesh"
   return (
     <>
-      <h1>Vite react app</h1>
-      <h1>Vite react app</h1>
+      <h1>Vite react {" "} app {2+2}</h1>
+      <h1>Vite react app {} </h1>
       <Youtube />
     </>
   )
